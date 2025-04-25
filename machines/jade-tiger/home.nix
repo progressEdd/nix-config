09f9e7e4d4bddecfaa7ae2acde33e4fd8f536c53
bedhedd <zbedhedd@gmail.com>
2025-04-25 17:25:01 -0500

    reorganize kde and plasma config

--- machines/jade-tiger/home.nix
+++ machines/jade-tiger/home.nix
@@ -3,38 +3,11 @@
 
 {
   # import your global home.nix (fonts, fish, etc.)
-  imports = [ ../../modules/home.nix ];
+  imports = [ 
+    ../../modules/home.nix 
+    ../../modules/kde-home.nix 
 
-  # now configure plasma via the plasma-manager HM module:
-  programs.plasma.enable = true;
-
-  programs.plasma.workspace = {
-    clickItemTo = "select";
-    lookAndFeel = "org.kde.breezedark.desktop";
-    # cursor.theme = "Bibata-Modern-Ice";
-    # iconTheme   = "Papirus-Dark";
-    # wallpaper   = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Patak/contents/images/1080x1920.png";
-  };
-
-  programs.plasma.hotkeys.commands."launch-konsole" = {
-    name    = "Launch Konsole";
-    key     = "Meta+Alt+K";
-    command = "konsole";
-  };
-
-  programs.plasma.panels = [
-    {
-      location = "bottom";
-      widgets = [
-        "org.kde.plasma.kickoff"
-        "org.kde.plasma.icontasks"
-        "org.kde.plasma.marginsseparator"
-        "org.kde.plasma.systemtray"
-        "org.kde.plasma.digitalclock"
-      ];
-    }
   ];
 
-  # …more programs.plasma.* settings as desired…
 }
 
---
+++ modules/kde-home.nix
@@ -0,0 +1,34 @@
+# modules/kde-home.nix
+{ pkgs, plasma-manager, ... }:
+
+{
+  programs.plasma = {
+    enable = true;
+
+    workspace = {
+      clickItemTo = "select";
+      lookAndFeel  = "org.kde.breezedark.desktop";
+      # …
+    };
+
+    hotkeys.commands."launch-konsole" = {
+      name    = "Launch Konsole";
+      key     = "Meta+Alt+K";
+      command = "konsole";
+    };
+
+    panels = [
+      {
+        location = "bottom";
+        widgets  = [
+          "org.kde.plasma.kickoff"
+          "org.kde.plasma.icontasks"
+          "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+        ];
+      }
+    ];
+  };
+}
+
