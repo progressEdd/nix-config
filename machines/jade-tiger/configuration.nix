fe1637a7ad60f8db66225a8ce5dff07818681e7f
bedhedd <zbedhedd@gmail.com>
2025-05-12 23:26:57 -0500

    move system packages to home module

--- machines/jade-tiger/configuration.nix
+++ machines/jade-tiger/configuration.nix
@@ -136,13 +136,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    git
-    wget
-    wl-clipboard
-    xclip
-  ];
+
   # Some programs need SUID wrappers, can be configured further or are:
   # started in user sessions.
   # programs.mtr.enable = true;
--- modules/home.nix
+++ modules/home.nix
@@ -25,7 +25,6 @@
 
       # system packages
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-      git
       wget
       wl-clipboard
       xclip
