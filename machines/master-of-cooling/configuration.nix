37d5a1cf14d1f0801da1cf5ca91fcdde4f36600f
bedhedd <zbedhedd@gmail.com>
2025-06-29 19:50:44 -0500

    add windows boot

--- machines/master-of-cooling/configuration.nix
+++ machines/master-of-cooling/configuration.nix
@@ -16,11 +16,20 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    extraEntries."windows.conf" = ''
+      title  Windows 11
+      efi    /EFI/Microsoft/Boot/bootmgfw.efi
+      sort-key windows   # keeps it near the top; optional
+    '';
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+
 
   networking.hostName = "master-of-cooling"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
