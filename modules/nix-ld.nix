aee89818cb928e155b3126d98ac304043d3c28dd
bedhedd <zbedhedd@gmail.com>
2025-06-30 03:52:05 -0500

    remove nix-ld module from modules folder since flake takes care of it now

--- modules/nix-ld.nix
+++
@@ -1,7 +0,0 @@
-# modules/nix-ld.nix
-{ pkgs, ... }: {
-  programs.nix-ld.enable = true;
-
-  # Optional: extra libs for closed-source binaries
-  # programs.nix-ld.libraries = with pkgs; [ zlib openssl ];
-}
