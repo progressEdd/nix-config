e2044665ff95a649b4fa6dccf10f683e0c1e7737
bedhedd <zbedhedd@gmail.com>
2025-06-30 02:34:17 -0500

    add night light

--- modules/kde-home.nix
+++ modules/kde-home.nix
@@ -6,6 +6,30 @@
     enable = true;
     workspace.lookAndFeel = lib.mkForce "com.valve.vapor.deck.desktop";
 
+    kwin.nightLight = {
+      enable = true;
+
+      # Pick ONE of the following modes ↓
+
+      ## 1. Always-on (“constant”)
+      # mode = "constant";
+
+      ## 2. Follow sunrise/sunset for your place (“location”)
+      mode = "location";
+      location.latitude  = "41.8781";   # ≈ Chicago – tweak if you’re elsewhere
+      location.longitude = "-87.6298";
+
+      ## 3. Fixed schedule (“times”)
+      # mode = "times";
+      # time.morning = "06:30";  # when Night Light turns *off*
+      # time.evening = "19:30";  # when it turns *on*
+
+      # Optional fine-tuning
+      temperature.day   = 6500;  # K
+      temperature.night = 4500;  # K
+      transitionTime    = 30;    # minutes for the fade
+      };
+
     panels = lib.mkForce [
       {
         screen = "all";
