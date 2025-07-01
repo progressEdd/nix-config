377812f3fdde521299d3cfc0f55886bdf0dd5e8e
bedhedd <zbedhedd@gmail.com>
2025-06-30 22:49:43 -0500

    move playwright vars to fish

--- modules/home.nix
+++ modules/home.nix
@@ -55,11 +55,6 @@
   home.sessionVariables = {          # <── needs the `home.` prefix
     LD_LIBRARY_PATH =
       "${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ]}:$LD_LIBRARY_PATH";
-    PLAYWRIGHT_BROWSERS_PATH =
-      "${pkgs.playwright-driver.browsers}";
-    
-    PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = 
-      "true";
   };
   
   #nix = {
