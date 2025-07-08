b9505e33b95da4925a124ad7eda3b2cf6007808e
bedhedd <zbedhedd@gmail.com>
2025-07-08 13:55:41 -0500

    add tlp to laptop

--- machines/think-nix/configuration.nix
+++ machines/think-nix/configuration.nix
@@ -107,7 +107,28 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+  tlp
   ];
+  
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+      # Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
