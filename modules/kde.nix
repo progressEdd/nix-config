4720ff584ff35869fe7b4e0a5950bd3b0bb7d357
bedhedd <zbedhedd@gmail.com>
2025-07-09 12:21:17 -0500

    make kde system level tweeks in kde.nix

--- modules/kde.nix
+++ modules/kde.nix
@@ -14,5 +14,33 @@
      kdePackages.kate         # Qt6-based Kate
      # more KDE apps, e.g. kdePackages.okular, etc.
    ];
+
+ # 1) Drop the desktop file globally
+  environment.etc."xdg/applications/vscodium-folder.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=VSCodium (Folder)
+    Exec=codium %F
+    Icon=codium
+    MimeType=inode/directory;
+    NoDisplay=true
+  '';
+
+  # 2) Tell XDG to add it to the “Open With…” list
+  xdg.mime = {
+    enable = true;
+
+    # keep dolphin if you like…
+    defaultApplications = {
+      "inode/directory" = [ "org.kde.dolphin.desktop" ];
+    };
+
+    # …but also add codium
+    addedAssociations = {
+      "inode/directory" = [ "vscodium-folder.desktop" ];
+    };
+
+  };
+ 
 }
 
