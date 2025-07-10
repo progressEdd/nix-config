27801211cd6bd808439e9c919e0e505af2dbdcb2
bedhedd <zbedhedd@gmail.com>
2025-07-10 15:33:18 -0500

    add universal.nix

--- modules/universal.nix
+++ modules/universal.nix
@@ -2,10 +2,13 @@
 { lib, ... }:
 
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nixpkgs.config.allowUnfree         = true;
-
-  # Home-Manager boiler-plate that applies everywhere
-  home-manager.useGlobalPkgs   = true;
-  home-manager.useUserPackages = true;
+    time.timeZone        = "America/Chicago";
+    i18n.defaultLocale   = "en_US.UTF-8";
+    
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nixpkgs.config.allowUnfree         = true;
+    
+    # Home-Manager boiler-plate that applies everywhere
+    home-manager.useGlobalPkgs   = true;
+    home-manager.useUserPackages = true;
 }
