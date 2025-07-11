1ea4a4c5017675fea174c3261cf26c9780b5f5f1
bedhedd <zbedhedd@gmail.com>
2025-07-11 15:50:39 -0500

    add generic user config

---
+++ users/generic-user.nix
@@ -0,0 +1,27 @@
+# users/generic-user.nix
+{ config, pkgs, home-manager, plasma-manager, lib, ... }:
+
+let
+  username     = "__USERNAME__";   # ← token the script will replace
+  userPackages = with pkgs; [ … ];
+in
+{
+  users.extraUsers.${username} = {
+    isNormalUser = true;
+    home         = "/home/${username}";
+    extraGroups  = [ "wheel" ];
+  };
+
+  home-manager.users.${username} = {
+    home.username      = username;
+    home.homeDirectory = "/home/${username}";
+    imports = [
+      ../modules/kde-home.nix
+      ../modules/development.nix
+      ../modules/guake.nix
+      ../dotfiles/multiple-ssh.nix
+    ];
+    programs.fish.enable = true;
+    home.packages        = userPackages;
+  };
+}
