bc450dd67e52f10b4b6c06ecb8758b5256b74564
bedhedd <zbedhedd@gmail.com>
2025-07-11 23:38:43 -0500

    add linux to default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,7 @@
 # modules/default.nix
 {
   universal = ./universal.nix;
+  linux     = ./linux.nix;
   kde       = ./kde.nix;
   kdeHome   = ./kde-home.nix;
   steamdeck = ./steamdeck-plasma-system.nix;
