790ab824109435dc66c440794b5e90106c0aa3dc
bedhedd <zbedhedd@gmail.com>
2025-07-11 23:54:54 -0500

    fix home-manager import

--- flake.nix
+++ flake.nix
@@ -1,52 +1,36 @@
 {
-  ########################################################################
-  # 1  Inputs                                                             #
-  ########################################################################
+# ── flake.nix ─────────────────────────────────────────────────────────────────
+{
   inputs = {
     nixpkgs.url        = "github:NixOS/nixpkgs/nixos-unstable";
-
     home-manager.url   = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     plasma-manager.url = "github:nix-community/plasma-manager";
-    plasma-manager.inputs.nixpkgs.follows   = "nixpkgs";
+    plasma-manager.inputs.nixpkgs.follows     = "nixpkgs";
     plasma-manager.inputs.home-manager.follows = "home-manager";
-
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
   outputs = { self, nixpkgs, home-manager, plasma-manager, nixos-hardware, ... }:
-  ########################################################################
-  # 2  Helper: enumerate host folders                                    #
-  ########################################################################
   let
-    hostNames =
-      builtins.filter
-        (name: builtins.pathExists (./hosts/${name}/default.nix))
-        (builtins.attrNames (builtins.readDir ./hosts));
-
-    # If you need per-host system types, put a `system = "x86_64-linux";`
-    # line in each hosts/<name>/default.nix and read it here with import.
     defaultSystem = "x86_64-linux";
-
-    modules = import ./modules;   # attr-set with .universal .linux …
-
-    mkSpecialArgs = {
-      inherit home-manager plasma-manager nixos-hardware modules;
-    };
-
-  ########################################################################
-  # 3  Build nixosConfigurations attr-set                                #
-  ########################################################################
-    nixosCfgs =
+    modules       = import ./modules;  # your universal & linux modules
+    mkArgs        = { inherit modules home-manager plasma-manager nixos-hardware; };
+    hostNames     = builtins.filter
+                      (n: builtins.pathExists ./hosts/${n}/default.nix)
+                      (builtins.attrNames (builtins.readDir ./hosts));
+  in {
+    nixosConfigurations =
       nixpkgs.lib.genAttrs hostNames (host:
         nixpkgs.lib.nixosSystem {
           system  = defaultSystem;
           modules =
             [ ./hosts/${host}/default.nix ]
             ++ nixpkgs.lib.optionals (defaultSystem == "x86_64-linux") [ modules.kde ];
-          specialArgs = mkSpecialArgs // { inherit host; };
+          specialArgs = mkArgs // { inherit host; };
         });
+  };
+}
 
   ########################################################################
   # 4  setup-wizard app (unchanged)                                       #
