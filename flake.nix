dc92a192e166fe8a691f9e53852df3781d81a19b
bedhedd <zbedhedd@gmail.com>
2025-07-11 23:59:33 -0500

    fix let scope

--- flake.nix
+++ flake.nix
@@ -1,7 +1,4 @@
 {
-  ########################################################################
-  # 1  Inputs                                                             #
-  ########################################################################
   inputs = {
     nixpkgs.url        = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url   = "github:nix-community/home-manager";
@@ -12,55 +9,34 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
-  ########################################################################
-  # 2  Outputs                                                            #
-  ########################################################################
   outputs = { self, nixpkgs, home-manager, plasma-manager, nixos-hardware, ... }:
 
   let
-    # a) find any sub-folder under ./hosts that has a default.nix
-    hostNames =
-      builtins.filter
-        (name: builtins.pathExists ./hosts/${name}/default.nix)
-        (builtins.attrNames (builtins.readDir ./hosts));
+    # your hosts:
+    hostNames     = builtins.filter
+                      (n: builtins.pathExists ./hosts/${n}/default.nix)
+                      (builtins.attrNames (builtins.readDir ./hosts));
 
-    # b) your default system for all hosts
     defaultSystem = "x86_64-linux";
 
-    # c) your shared modules set (universal, linux, kde, etc.)
-    modules    = import ./modules;
+    modules       = import ./modules;
+    mkSpecialArgs = { inherit modules home-manager plasma-manager nixos-hardware; };
 
-    # d) the four things you want every host to see
-    mkSpecialArgs = {
-      inherit modules home-manager plasma-manager nixos-hardware;
-    };
+    # ← move it here
+    systems       = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
   in
   {
-    ########################################################################
-    # 3  NixOS configurations                                               #
-    ########################################################################
-    nixosConfigurations =
-      nixpkgs.lib.genAttrs hostNames (host:
-        nixpkgs.lib.nixosSystem {
-          system  = defaultSystem;
-
-          # each host just points at its own default.nix, plus `modules.kde` on Linux
-          modules =
-            [ ./hosts/${host}/default.nix ]
-            ++ nixpkgs.lib.optionals (defaultSystem == "x86_64-linux") [ modules.kde ];
-
-          # inject your four flakes + the host name itself
-          specialArgs = mkSpecialArgs // { inherit host; };
-        });
-
-    ########################################################################
-    # 4  setup-wizard app                                                    #
-    ########################################################################
-    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    nixosConfigurations = nixpkgs.lib.genAttrs hostNames (host:
+      nixpkgs.lib.nixosSystem {
+        system     = defaultSystem;
+        modules    = [ ./hosts/${host}/default.nix ]
+                     ++ nixpkgs.lib.optionals (defaultSystem == "x86_64-linux") [ modules.kde ];
+        specialArgs = mkSpecialArgs // { inherit host; };
+      });
 
     apps = nixpkgs.lib.genAttrs systems (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs      = import nixpkgs { inherit system; };
         scriptDrv = pkgs.writeShellScriptBin "setup-wizard" ''
           #!${pkgs.runtimeShell}
           export PATH=${pkgs.pciutils}/bin:$PATH
