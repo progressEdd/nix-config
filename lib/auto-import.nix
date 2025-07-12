7050326c996deb79fc94e79f56cb6b7c1e40acd8
bedhedd <zbedhedd@gmail.com>
2025-07-12 02:29:17 -0500

    reorganize files

--- lib/auto-import.nix
+++
@@ -1,5 +0,0 @@
-# lib/autoImport.nix
-dir: let
-  files = builtins.attrNames (builtins.readDir dir);
-  nixFiles = builtins.filter (n: lib.hasSuffix ".nix" n) files;
-in map (n: dir + "/${n}") nixFiles
--- machines/generic-machine/users.nix
+++
@@ -1,8 +0,0 @@
-{ ... }: {
-  imports = [
-    ../../users/admin.nix
-    ../../users/dev.nix
-    ../../users/bedhedd.nix
-  ];
-}
-
