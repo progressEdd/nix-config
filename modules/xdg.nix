4cd3ba61e62c47900cb45ed5e62d93ad68bff82f
bedhedd <zbedhedd@gmail.com>
2025-10-18 13:08:11 -0500

    create separte profiles

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -19,17 +19,17 @@
 
   # Separate launchers (menu items you can pin to the panel)
   xdg.desktopEntries = {
-    librewolf-personal = {
-      name = "LibreWolf (Personal)";
-      comment = "LibreWolf with the Personal profile";
-      exec = ''librewolf --name librewolf-personal -P personal --no-remote --new-instance %u'';
+    librewolf-master = {
+      name = "LibreWolf (Master)";
+      comment = "LibreWolf with the Master profile";
+      exec = ''librewolf --name librewolf-master -P master --no-remote --new-instance %u'';
       icon = "librewolf";
       terminal = false;
       type = "Application";
       categories = [ "Network" "WebBrowser" ];
       mimeType = [ "text/html" "x-scheme-handler/http" "x-scheme-handler/https" ];
       settings = {
-        X-KDE-WaylandAppId = "librewolf-personal";
+        X-KDE-WaylandAppId = "librewolf-master";
       };
     };
 
@@ -44,6 +44,34 @@
       mimeType = [ "text/html" "x-scheme-handler/http" "x-scheme-handler/https" ];
       settings = {
         X-KDE-WaylandAppId = "librewolf-professional";
+      };
+    };
+
+    librewolf-personal = {
+      name = "LibreWolf (Personal)";
+      comment = "LibreWolf with the Personal profile";
+      exec = ''librewolf --name librewolf-personal -P personal --no-remote --new-instance %u'';
+      icon = "librewolf";
+      terminal = false;
+      type = "Application";
+      categories = [ "Network" "WebBrowser" ];
+      mimeType = [ "text/html" "x-scheme-handler/http" "x-scheme-handler/https" ];
+      settings = {
+        X-KDE-WaylandAppId = "librewolf-personal";
+      };
+    };
+
+    librewolf-lan-management = {
+      name = "LibreWolf (Lan-Management)";
+      comment = "LibreWolf with the Lan-Management profile";
+      exec = ''librewolf --name librewolf-lan-management -P lan-management --no-remote --new-instance %u'';
+      icon = "librewolf";
+      terminal = false;
+      type = "Application";
+      categories = [ "Network" "WebBrowser" ];
+      mimeType = [ "text/html" "x-scheme-handler/http" "x-scheme-handler/https" ];
+      settings = {
+        X-KDE-WaylandAppId = "librewolf-lan-management";
       };
     };
 
