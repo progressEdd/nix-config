a93386aa885f7bd90662139d74a54116471095f9
bedhedd <zbedhedd@gmail.com>
2025-10-18 12:34:23 -0500

    create separate windows

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -20,31 +20,25 @@
   # Separate launchers (menu items you can pin to the panel)
   xdg.desktopEntries = {
     librewolf-work = {
-      name = "LibreWolf (professional)";
-      comment = "LibreWolf with the professional profile";
-      exec = "librewolf --class LibreWolf-professional -P professional -no-remote --new-instance %u";
+      name = "LibreWolf (Professional)";
+      exec = ''env MOZ_ENABLE_WAYLAND=0 librewolf --class LibreWolf-Professional -P Work -no-remote --new-instance %u'';
       icon = "librewolf";
       terminal = false;
       type = "Application";
       categories = [ "Network" "WebBrowser" ];
       mimeType = [ "text/html" "x-scheme-handler/http" "x-scheme-handler/https" ];
-      settings = {
-        StartupWMClass = "LibreWolf-professional";
-      };
+      settings = { StartupWMClass = "LibreWolf-Work"; };
     };
 
     librewolf-personal = {
-      name = "LibreWolf (personal)";
-      comment = "LibreWolf with the personal profile";
-      exec = "librewolf --class LibreWolf-personal -P personal -no-remote --new-instance %u";
+      name = "LibreWolf (Personal)";
+      exec = ''env MOZ_ENABLE_WAYLAND=0 librewolf --class LibreWolf-Personal -P Personal -no-remote --new-instance %u'';
       icon = "librewolf";
       terminal = false;
       type = "Application";
       categories = [ "Network" "WebBrowser" ];
       mimeType = [ "text/html" "x-scheme-handler/http" "x-scheme-handler/https" ];
-      settings = {
-        StartupWMClass = "LibreWolf-personal";
-      };
+      settings = { StartupWMClass = "LibreWolf-Personal"; };
     };
 
     # Example override you already had
