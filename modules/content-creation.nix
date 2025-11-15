6985029248324c28f83380719d62a8bf98f10a01
bedhedd <zbedhedd@gmail.com>
2025-11-15 15:31:59 -0600

    comment out obs so that the custom moudle takes care of it

--- modules/content-creation.nix
+++ modules/content-creation.nix
@@ -2,7 +2,7 @@
 
 {
   home.packages = with pkgs; [
-    obs-studio # screen recording
+    # obs-studio # screen recording
     obs-studio-plugins.obs-vkcapture # Game capture
     obs-studio-plugins.advanced-scene-switcher # scene switcher
     # obs-studio-plugins.obs-multi-rtmp # simulcasting/multistream
