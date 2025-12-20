c5111c3dffe984102dc7de23f72ba4b6bea68a20
progressEdd <progressEdd@gmail.com>
2025-12-20 14:23:29 -0600

    make universal.nix truely cross platform

--- modules/universal.nix
+++ modules/universal.nix
@@ -1,20 +1,16 @@
 # modules/universal.nix
-{ lib, ... }:
+{ ... }:
 
 {
-    time.timeZone        = "America/Chicago";
-    i18n.defaultLocale   = "en_US.UTF-8";
-    
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-    nixpkgs.config.allowUnfree         = true;
-    
-    # Home-Manager boiler-plate that applies everywhere
-    home-manager.useGlobalPkgs   = true;
-    home-manager.useUserPackages = true;
+  time.timeZone = "America/Chicago";
 
-    home-manager.sharedModules = [
-    ../modules/home.nix        # ← your “global home” module
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+
+  home-manager.sharedModules = [
+    ../modules/home.nix
   ];
-
-  programs.nix-ld.enable = true;
 }
