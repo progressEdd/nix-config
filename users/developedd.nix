56e5fb8af6c11ef08abe9dc5be9f91dd9fab5a56
progressEdd <progressEdd@gmail.com>
2025-12-20 15:35:19 -0600

    temp allow home manager to work on mac

--- users/developedd.nix
+++ users/developedd.nix
@@ -1,44 +1,27 @@
-# users/developedd.nix
-{ config, pkgs, home-manager, plasma-manager, lib, ... }:
+# users/developedd.nix  (Home Manager module)
+{ pkgs, lib, ... }:
 
 let
-  username     = "developedd";
-  userPackages = with pkgs; [
-    # add account specific packages here
+  username = "developedd";
+  homeDir = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
+in
+{
+  home.username = username;
+  home.homeDirectory = homeDir;
+
+  imports = [
+    ../modules/development.nix
+    ../dotfiles/multiple-ssh.nix
+  ];
+
+  home.file.".config/karabiner/karabiner.json".source = ../dotfiles/karabiner.json;
+
+  programs.fish.enable = true;
+
+  home.packages = with pkgs; [
     colima
     code-cursor
     ollama
     obs-studio
   ];
-  
-  # Determine home directory based on OS
-  homeDir = if pkgs.stdenv.isDarwin 
-            then "/Users/${username}"
-            else "/home/${username}";
-in
-{
-  users.users.${username} = lib.mkMerge [
-    # Linux-specific config
-    (lib.mkIf pkgs.stdenv.isLinux {
-      isNormalUser = true;
-      home         = homeDir;
-      extraGroups  = [ "wheel" ];
-    })
-    # macOS-specific config
-    (lib.mkIf pkgs.stdenv.isDarwin {
-      home = homeDir;
-    })
-  ];
-
-  home-manager.users.${username} = {
-    home.username      = username;
-    home.homeDirectory = homeDir;
-    imports = [
-      ../modules/development.nix
-      ../dotfiles/multiple-ssh.nix
-    ];
-    home.file.".config/karabiner/karabiner.json".source = ../dotfiles/karabiner.json;
-    programs.fish.enable = true;
-    home.packages        = userPackages;
-  };
 }
